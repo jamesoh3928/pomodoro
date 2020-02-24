af4467ee4f0599382536a86d243422e40504c804
James <jo9347@rit.edu>
2020-02-24 11:48:40 +0000

    Currently made stopwatch, it will be changed to timer later

--- app.js
+++ app.js
@@ -1,50 +1,88 @@
-var $dOut = $('#date'),
-    $hOut = $('#hours'),
-    $mOut = $('#minutes'),
-    $sOut = $('#seconds'),
-    $ampmOut = $('#ampm');
+var dOut = $('#date');
 var months = [
-  'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
 ];
+var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-var days = [
-  'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'
-];
+var start, stop, clock;
 
-function update(){
-  var date = new Date();
-  
-  var ampm = date.getHours() < 12
-             ? 'AM'
-             : 'PM';
-  
-  var hours = date.getHours() == 0
-              ? 12
-              : date.getHours() > 12
-                ? date.getHours() - 12
-                : date.getHours();
-  
-  var minutes = date.getMinutes() < 10 
-                ? '0' + date.getMinutes() 
-                : date.getMinutes();
-  
-  var seconds = date.getSeconds() < 10 
-                ? '0' + date.getSeconds() 
-                : date.getSeconds();
-  
-  var dayOfWeek = days[date.getDay()];
-  var month = months[date.getMonth()];
-  var day = date.getDate();
-  var year = date.getFullYear();
-  
-  var dateString = dayOfWeek + ', ' + month + ' ' + day + ', ' + year;
-  
-  $dOut.text(dateString);
-  $hOut.text(hours);
-  $mOut.text(minutes);
-  $sOut.text(seconds);
-  $ampmOut.text(ampm);
-} 
+// Grab the necessary elements from the DOM
+start = document.getElementById('start');
+stop = document.getElementById('stop');
+clock = document.getElementById('clock');
+
+// Add event listeners to both buttons
+start.addEventListener('click', function() {
+    stopwatch('start');
+});
+
+stop.addEventListener('click', function() {
+    stopwatch('stop');
+});
+
+// Define a timer ID for the setInterval function
+timerId = null;
+
+// Create the stopwatch
+function stopwatch(command) {
+    var hours = 0,
+        minutes = 0,
+        seconds = 0,
+        display;
+
+    function makeTwoDigits(number) {
+        // display double digits for numbers less than 10
+        if (number < 10) {
+            return '0' + number;
+        }
+
+        return number;
+    }
+
+    if (command === 'start') {
+        timerId = setInterval(function() {
+            seconds++;
+            if (seconds === 60) {
+                seconds = 0;
+                minutes++;
+            }
+
+            if (minutes === 60) {
+                minutes = 0;
+                hours++;
+            }
+
+            display =
+                makeTwoDigits(hours) + ':' + makeTwoDigits(minutes) + ':' + makeTwoDigits(seconds);
+            clock.innerHTML = display;
+            console.log(display);
+        }, 1000);
+    } else if (command === 'stop') {
+        clearInterval(timerId);
+    }
+}
+
+function update() {
+    var date = new Date();
+    var dayOfWeek = days[date.getDay()];
+    var month = months[date.getMonth()];
+    var day = date.getDate();
+    var year = date.getFullYear();
+    var dateString = dayOfWeek + ', ' + month + ' ' + day + ', ' + year;
+
+    dOut.text(dateString);
+}
 
 update();
 window.setInterval(update, 1000);
